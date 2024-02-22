e600960a3d142ba7df3a5cd3214f9ad566899288
Asura <eper.karma@gmail.com>
2024-02-22 08:45:43 +0100

    pog?

---
+++ MTG_Card_Printer/Page.cs
@@ -0,0 +1,16 @@
+﻿namespace MyApp
+{
+    internal class Page
+    {
+        private object letter;
+        private object portrait;
+        private float v;
+
+        public Page(object letter, object portrait, float v)
+        {
+            this.letter = letter;
+            this.portrait = portrait;
+            this.v = v;
+        }
+    }
+}
--- MTG_Card_Printer/Program.cs
+++ MTG_Card_Printer/Program.cs
@@ -1,3 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection.Emit;
+using System.Reflection.Metadata;
 
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        static void Main(string[] args)
+        {
+        }
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+    }
+}
